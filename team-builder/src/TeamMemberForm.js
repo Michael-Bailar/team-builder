442065e8cc0b205553405f05aa32eac263fe132b
Michael-B1764 <mikebailar@gmail.com>
2020-02-27 13:41:10 -0800

    just checking that everything is uploaded.

--- team-builder/src/TeamMemberForm.js
+++ team-builder/src/TeamMemberForm.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 
 const TeamMemberForm = props => {
-    console.log("props", props);
     const [teamMember, setTeamMember] = useState({
       name: "",
       email: "",
